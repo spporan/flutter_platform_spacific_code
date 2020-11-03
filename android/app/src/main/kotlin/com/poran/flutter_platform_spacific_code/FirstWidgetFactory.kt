34ad1252b46b2930a88cd6d64aa219d2baeaa36d
ridmik <poran.cse@gmail.com>
2020-11-03 15:39:28 +0600

    Created FirstWidgetFactory.kt

---
+++ android/app/src/main/kotlin/com/poran/flutter_platform_spacific_code/FirstWidgetFactory.kt
@@ -0,0 +1,15 @@
+package com.poran.flutter_platform_spacific_code
+
+import android.content.Context
+import android.view.View
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.StandardMessageCodec
+import io.flutter.plugin.platform.PlatformView
+import io.flutter.plugin.platform.PlatformViewFactory
+
+internal class FirstWidgetFactory(private var messenger: BinaryMessenger) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+    override fun create(context: Context, id: Int, args: Any?): PlatformView {
+        val creationParams = args as Map<String?, Any?>?
+        return FirstWidget(context, id, creationParams,messenger)
+    }
+}
