423ac97036b190d8cf0b2dfc7e0bca32681ff932
ridmik <poran.cse@gmail.com>
2020-11-03 15:41:07 +0600

    Called FirstWidgetPlugin from MainActivity.kt

--- android/app/src/main/kotlin/com/poran/flutter_platform_spacific_code/MainActivity.kt
+++ android/app/src/main/kotlin/com/poran/flutter_platform_spacific_code/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.poran.flutter_platform_spacific_code
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        FirstWidgetPlugin.registerWith( flutterEngine)
+
+    }
 }
